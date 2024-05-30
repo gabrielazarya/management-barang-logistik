16b66b51a828d54ea07f08c6d13f4e53f933b5be
unknown <gabrielkrishna5@gmail.com>
2024-05-30 20:58:34 +0700

    Update Guest view

--- resources/views/view-guest/includes/landingpage.blade.php
+++ resources/views/view-guest/includes/landingpage.blade.php
@@ -1,36 +1,6 @@
 {{-- Landing Page --}}
 
-@if (Request::is('informasi'))
-    <div class="landing-page-2">
-        <div class="background-image">
-            <div class="dark-overlay"></div>
-        </div>
-        <div class="content-2 konten">
-            <h2 class="judul-home">Informasi Barang Logistik</h2>
-            <p>Informasi barang logistik Telkom University Surabaya</p>
-        </div>
-    </div>
-@elseif(Request::is('pinjam'))
-    <div class="landing-page-2">
-        <div class="background-image">
-            <div class="dark-overlay"></div>
-        </div>
-        <div class="content-2 konten">
-            <h2 class="judul-home">Pinjam Barang Logistik</h2>
-            <p>Pinjam barang logistik Telkom University Surabaya</p>
-        </div>
-    </div>
-@elseif(Request::is('profil'))
-    <div class="landing-page-2">
-        <div class="background-image">
-            <div class="dark-overlay"></div>
-        </div>
-        <div class="content-2 konten">
-            <h2 class="judul-home">Profil</h2>
-            <p>Profil Peminjam barang logistik Telkom University Surabaya</p>
-        </div>
-    </div>
-@elseif(Request::is('tentang'))
+@if (Request::is('tentang'))
     <div class="landing-page-2">
         <div class="background-image">
             <div class="dark-overlay"></div>
@@ -38,6 +8,7 @@
         <div class="content-2 konten">
             <h2 class="judul-home">Tentang Kami</h2>
             <p>Tentang Peminjaman barang logistik Telkom University Surabaya</p>
+            <a href="/pinjam" class="btn">Pinjam Barang</a>
         </div>
     </div>
 @else
