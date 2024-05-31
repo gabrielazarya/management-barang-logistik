c55ed7c1b11953336d5b0255dd07abb3dbfa617f
unknown <hafiz.hartawan@gmail.com>
2024-05-31 11:20:42 +0700

    add style di halaman ketersediaan

--- resources/views/view-user/ketersediaan.blade.php
+++ resources/views/view-user/ketersediaan.blade.php
@@ -9,9 +9,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <div class="container mt-5">
-                        <h1 class="mb-4">Ketersediaan Barang</h1>
-                        <form method="GET" action="{{ route('ketersediaan') }}">
+                    <div class="ketersediaan-container mt-5">
+                        <h1 class="ketersediaan-header mb-4">Ketersediaan Barang</h1>
+                        <form class="ketersediaan-form" method="GET" action="{{ route('ketersediaan') }}">
                             <label for="tanggal_pinjam">Tanggal Pinjam:</label>
                             <input type="date" id="tanggal_pinjam" name="tanggal_pinjam" value="{{ $tanggal_pinjam }}">
                         
@@ -21,29 +21,31 @@
                             <button type="submit" class="btn btn-sm btn-primary">Cek Ketersediaan</button>
                         </form>
                         <div class="table-responsive">
-                        <table class="table table-striped">
-                            <thead>
-                                <tr>
-                                    <th>ID Barang</th>
-                                    <th>Nama Barang</th>
-                                    <th>Tipe Barang</th>
-                                    <th>Jumlah Tersedia</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($items as $item)
+                            <table class="ketersediaan-table table table-striped">
+                                <thead>
                                     <tr>
-                                        <td>{{ $item->id_barang }}</td>
-                                        <td>{{ $item->nama_barang }}</td>
-                                        <td>{{ $item->tipe_barang }}</td>
-                                        <td>{{ $item->jumlah_tersedia }}</td>
+                                        <th>ID Barang</th>
+                                        <th>Nama Barang</th>
+                                        <th>Tipe Barang</th>
+                                        <th>Jumlah Tersedia</th>
                                     </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody>
+                                    @foreach ($items as $item)
+                                        <tr>
+                                            <td>{{ $item->id_barang }}</td>
+                                            <td>{{ $item->nama_barang }}</td>
+                                            <td>{{ $item->tipe_barang }}</td>
+                                            <td>{{ $item->jumlah_tersedia }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <link rel="stylesheet" href="{{ asset('css/ketersediaan.css') }}">
 </x-app-layout>
